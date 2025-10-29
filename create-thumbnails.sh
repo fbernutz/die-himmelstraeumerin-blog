c6a97103914b7cefec10c72a4e7905a64c276fed
Felizia Bernutz <felizia.bernutz@gmx.de>
2025-10-29 11:25:43 +0100

    Update thumbnail creation script for performance

--- create-thumbnails.sh
+++ create-thumbnails.sh
@@ -1,14 +1,23 @@
 #!/usr/bin/env bash
-
-# create a thumbnail with suffix '-small.jpg' for all jpg sketchnotes
+#
+# Generate thumbnails with suffix '-small.jpg' for sketchnote images, only if missing or outdated.
+#
 
 thumbnailSuffix='-small.jpg'
+imageDir="Resources/images/sketchnotes"
 
 GLOBIGNORE="*$thumbnailSuffix"
-for image in Resources/images/sketchnotes/*; do
-	echo "converting $image"
-	convert "$image" \
-		-resize 500 \
-		-quality 70 \
-		"${image%.jpg}$thumbnailSuffix"
+for image in "$imageDir"/*.jpg; do
+    thumbnail="${image%.jpg}$thumbnailSuffix"
+
+    # Skip if thumbnail exists and is newer than source
+    if [[ -f "$thumbnail" && "$thumbnail" -nt "$image" ]]; then
+        continue
+    fi
+
+    echo "converting $image"
+    convert "$image" \
+        -resize 500 \
+        -quality 70 \
+        "$thumbnail"
 done
